682a5e8834245cfc04e1dab5d303df599ed2b99b
Alan Marx <alan@zipline.inc>
2022-12-05 15:12:51 +0100

    Move summary methods into file class

--- 2022/lib/elf_calorie_file.rb
+++ 2022/lib/elf_calorie_file.rb
@@ -1,11 +1,27 @@
+require_relative './elf'
+
 class ElfCalorieFile
-  include Enumerable
+  attr_reader :path
 
   def initialize(path)
-    @lines = File.readlines(path).lazy.collect(&:chomp)
+    @path = path
   end
 
-  def each(&block)
-    @lines.each(&block)
+  def max_calories(num_of_elves)
+    sorted_elves.take(num_of_elves).sum(&:total_calories)
+  end
+
+  private
+
+  def sorted_elves
+    @sorted_elves ||= elves.sort_by(&:total_calories).reverse
+  end
+
+  def elves
+    @elves ||= lines.chunk_while { |_, next_line| !next_line.empty? }.collect.with_index(1) { |calories, number| Elf.new(number, calories) }
+  end
+
+  def lines
+    @lines ||= File.readlines(path).collect(&:chomp)
   end
 end
