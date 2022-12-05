4a8e20e1b3ec74f54056a7475184b4ab81e060e2
Alan Marx <alan@zipline.inc>
2022-12-05 14:28:04 +0100

    Refactor highest_total_calories method

--- 2022/lib/elf_calorie_hash.rb
+++ 2022/lib/elf_calorie_hash.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class ElfCalorieHash
   include Enumerable
 
@@ -7,10 +9,16 @@ class ElfCalorieHash
       .chunk_while { |_chunk, next_line| !next_line.empty? }
       .collect
       .with_index(1) { |calories, elf| [elf, calories.sum(&:to_i)] }
+      .sort { |elf, next_elf| elf.last <=> next_elf.last }
+      .reverse
       .to_h
   end
 
   def each(&block)
     @elf_calorie_hash.each(&block)
+  end
+
+  def highest_total_calories(elves = 1)
+    take(elves).sum(&:last)
   end
 end
